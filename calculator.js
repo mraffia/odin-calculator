c11eb29940854b8667d3b0e320b0791fa72d9116
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-02-18 14:48:12 +0700

    Add operate function

--- calculator.js
+++ calculator.js
@@ -1,15 +1,26 @@
-const add = function(num1, num2) {
+function add(num1, num2) {
 	return num1 + num2;
 };
 
-const subtract = function(num1, num2) {
+function subtract(num1, num2) {
 	return num1 - num2;
 };
-
-const multiply = function(num1, num2) {
+function multiply(num1, num2) {
     return num1 * num2;
 };
 
-const divide = function(num1, num2) {
+function divide(num1, num2) {
     return num1 / num2;
 };
+
+function operate(operator, num1, num2) {
+    if(operator === 'add') {
+        return add(num1, num2);
+    } else if(operator === 'subtract') {
+        return subtract(num1, num2);
+    } else if(operator === 'multiply') {
+        return multiply(num1, num2);
+    } else if(operator === 'divide') {
+        return divide(num1, num2);
+    }
+}
