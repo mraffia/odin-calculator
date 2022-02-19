f4d08780eb1a4daa5ecb083a7608072ff5b073f7
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-02-19 12:43:08 +0700

    Finish digits, operators, and equal sign logic

--- calculator.js
+++ calculator.js
@@ -25,19 +25,78 @@ function operate(operator, num1, num2) {
     }
 }
 
+// Source: https://stackoverflow.com/questions/32229667/have-max-2-decimal-places/32229831
+function toFixedIfNecessary(value, dp) {
+    return +parseFloat(value).toFixed(dp);
+}
+
 const container = document.querySelector('#container');
 const display = document.querySelector('.display');
+
 const backspace = document.querySelector('#backspace');
 const clear = document.querySelector('#clear');
+
 const digits = document.querySelectorAll('.digit');
 const operators = document.querySelectorAll('.operator');
 const equals = document.querySelector('#equals');
 
-let value = 0;
-let operation = '';
+let value1 = '';
+let value2 = '';
+let valueAfterEquals = '';
+let operatorValue = '';
 
 digits.forEach((digit) => {
     digit.addEventListener('click', (e) => {
-        display.textContent += e.target.id;
+        if(valueAfterEquals !== '') {
+            valueAfterEquals = '';
+            value1 += e.target.id;
+            display.textContent = e.target.id;
+        } else if(operatorValue === '') {
+            value1 += e.target.id;
+            display.textContent += e.target.id;
+        } else if(operatorValue !== '') {
+            value2 += e.target.id;
+            display.textContent += e.target.id;
+        }
     });
-})
+});
+
+operators.forEach((operator) => {
+    operator.addEventListener('click', (e) => {
+        if(valueAfterEquals !== '') {
+            value1 = valueAfterEquals;
+            valueAfterEquals = '';
+            operatorValue = e.target.id;
+            display.textContent += ' ' + e.target.textContent + ' ';
+        } else if(value2 === '') {
+            operatorValue = e.target.id;
+            display.textContent += ' ' + e.target.textContent + ' ';
+        } else if(value2 !== '') {
+            let total = 0;
+
+            total = operate(operatorValue, Number(value1), Number(value2));
+            total = toFixedIfNecessary(total, 3);
+
+            value1 = String(total);
+            value2 = '';
+            operatorValue = e.target.id;
+            display.textContent = total + ' ' + e.target.textContent + ' ';
+        }
+    });
+});
+
+equals.addEventListener('click', (e) => {
+    if(value2 === '' || operatorValue === '') {} 
+    else if(value2 !== '' && operatorValue !== '') {
+        let total = 0;
+
+        total = operate(operatorValue, Number(value1), Number(value2));
+        total = toFixedIfNecessary(total, 3);
+
+        value1 = '';
+        value2 = '';
+        valueAfterEquals = String(total);
+        operatorValue = '';
+        display.textContent = total;
+    }
+});
